19f7385c65b0bef3a31d31b6ef7a4619b7356075
Muso <musooff@gmail.com>
2019-04-07 22:50:57 +0900

    Added empty splash screen

---
+++ app/src/main/java/com/ballboycorp/tingting/base/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.ballboycorp.tingting.base
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by musooff on 07/04/2019.
+ */
+
+open class BaseActivity: AppCompatActivity() {
+}
---
+++ app/src/main/java/com/ballboycorp/tingting/splash/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.ballboycorp.tingting.splash
+
+import android.os.Bundle
+import com.ballboycorp.tingting.R
+import com.ballboycorp.tingting.base.BaseActivity
+
+/**
+ * Created by musooff on 07/04/2019.
+ */
+
+class SplashActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
