0fe16b52a04feccff6438feb5c83450ab4e0af5e
Muso <musooff@gmail.com>
2019-04-12 22:58:46 +0900

    Finished Profile Fragment UI

--- app/src/main/java/com/ballboycorp/tingting/main/home/HomeFragment.kt
+++ app/src/main/java/com/ballboycorp/tingting/main/home/HomeFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.ballboycorp.tingting.R
 import com.ballboycorp.tingting.base.BaseFragment
 import com.ballboycorp.tingting.databinding.FragmentHomeBinding
-import com.ballboycorp.tingting.main.home.adapter.HomeRecyclerViewAdapter
 import com.ballboycorp.tingting.main.home.adapter.ViewPagerAdapter
-import com.ballboycorp.tingting.main.home.utils.ItemDecorator
 import com.ballboycorp.tingting.qr.QRScanActivity
 import com.ballboycorp.tingting.utils.extensions.bind
 import com.ballboycorp.tingting.utils.extensions.getViewModel
